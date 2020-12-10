4bca7bd2704a1fa7a53cffecfd21bc21ffc13757
Yohanes Dwi Listio <yohanes@digiponic.co.id>
2020-12-10 21:41:37 +0700

    Membuat routes

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-  response.ok("Apliaksi REST API berjalan!");
+  response.ok("Aplikasi REST API berjalan!");
 };
