9668e3857ad84e08fd32a096f43d526971aa74ec
Yohanes Dwi Listio <yohanes@digiponic.co.id>
2020-12-11 08:27:48 +0700

    Menampilkan semua data dan data berdasarkan id

--- controller.js
+++ controller.js
@@ -6,3 +6,26 @@ var connection = require('./koneksi');
 exports.index = function(req, res) {
   response.ok("Aplikasi REST API berjalan!", res);
 };
+
+// Menampilkan data semua mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function(error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+// Menampilkan semua data mahasiswa berdasarkan id
+exports.tampilbasedonid = function (req, res) {
+  let id = req.params.id;
+  connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa = ?", [id], function(error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
   var json = require('./controller');
 
   app.route('/').get(json.index);
+  app.route('/tampil').get(json.tampilsemuamahasiswa);
+  app.route('/tampil/:id').get(json.tampilbasedonid);
 };
