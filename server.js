1a70cc503e06671abfab7826617cdac6c5c23b1f
Yohanes Dwi Listio <yohanes@digiponic.co.id>
2020-12-11 10:12:26 +0700

    Menambahkan registrasi

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 // Parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Memanggil routes
 var routes = require('./routes');
 routes(app);
 
+// Daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
+
 app.listen(3000, () => {
-  console.log(`Server started on port`);
+  console.log('Server started on port 3000');
 });
