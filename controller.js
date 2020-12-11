fec0e9e83a8463333e9c6493cf9654314bb60c15
Yohanes Dwi Listio <yohanes@digiponic.co.id>
2020-12-11 08:16:23 +0700

    Editing controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-  response.ok("Aplikasi REST API berjalan!");
+  response.ok("Aplikasi REST API berjalan!", res);
 };
